f7a1b4e5a7f39479154251414bf11e8bbb6b0975
Luke Barlow <luke.barlow@gmail.com>
2019-02-10 14:23:41 +0000

    test the querying of cover when deployed

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,6 +5,7 @@ import Piece from './Piece.js'
 export default ({ project }) => (
   <div>
     Project {project.frontmatter.title} <br />
+    cover {project.frontmatter.cover} <br />
     { project.frontmatter.pieces.map(piece => <Piece key={piece.title} piece={piece} />)}
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,46 +21,6 @@ export default class IndexPage extends React.Component {
     </div>
   }
 
-  // render() {
-  //   const { data } = this.props
-  //   const { edges: posts } = data.allMarkdownRemark
-
-  //   return (
-  //     <Layout>
-  //       <section className="section">
-  //         <div className="container">
-  //           <div className="content">
-  //             <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-  //           </div>
-  //           {posts
-  //             .map(({ node: post }) => (
-  //               <div
-  //                 className="content"
-  //                 style={{ border: '1px solid #333', padding: '2em 4em' }}
-  //                 key={post.id}
-  //               >
-  //                 <p>
-  //                   <Link className="has-text-primary" to={post.fields.slug}>
-  //                     {post.frontmatter.title}
-  //                   </Link>
-  //                   <span> &bull; </span>
-  //                   <small>{post.frontmatter.date}</small>
-  //                 </p>
-  //                 <p>
-  //                   {post.excerpt}
-  //                   <br />
-  //                   <br />
-  //                   <Link className="button is-small" to={post.fields.slug}>
-  //                     Keep Reading →
-  //                   </Link>
-  //                 </p>
-  //               </div>
-  //             ))}
-  //         </div>
-  //       </section>
-  //     </Layout>
-  //   )
-  // }
 }
 
 IndexPage.propTypes = {
@@ -82,6 +42,7 @@ export const pageQuery = graphql`
           frontmatter {
             title
             templateKey,
+            cover,
             pieces {
               title,
               description
