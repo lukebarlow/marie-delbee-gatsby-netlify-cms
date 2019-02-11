9759e9b9c01df6daa7c7d08329f3347d769d7e6f
Luke Barlow <luke.barlow@gmail.com>
2019-02-11 11:52:48 +0000

    movement between projects and pieces working

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-export default ({ piece }) => (
-  <div>&nbsp;&nbsp;Piece {piece.title}</div>
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  height: 100vh;
+  border: 1pt solid red;
+`
+export default  ({ piece }) => (
+  <StyledDiv>&nbsp;&nbsp;Piece {piece.title}</StyledDiv>
 )
+
