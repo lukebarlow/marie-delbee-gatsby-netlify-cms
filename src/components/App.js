6b8069e598841d2951d3a5a7be45c799afb7fbaa
Luke Barlow <luke.barlow@gmail.com>
2019-02-11 14:45:55 +0000

    working with scroll wheel

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import React from 'react'
 import styled from 'styled-components'
 import scroll from 'scroll'
 
-// import './all.sass'
 import '../styles/App.css'
 
 import Project from './Project'
@@ -12,6 +11,7 @@ const ProjectContainer = styled.div`
   height: 100vh;
   overflow: hidden;
 `
+const scrollTriggerThreshold = 51
 
 export default class App extends React.Component {
   constructor () {
@@ -19,14 +19,15 @@ export default class App extends React.Component {
     this.handleRef = this.handleRef.bind(this)
     this.projectIndex = 0
     this.pieceIndex = 0 // where 0 is the cover, and 1 is the first piece
+    this.isScrolling = false
   }
 
   componentDidMount () {
-
-    // const { data } = this.props
-    // let { edges: projects } = data.allMarkdownRemark
-
     const { projects } = this.props
+
+    const finishedScrolling = () => {
+      this.isScrolling = false
+    }
 
     const verticalScroll = () => {
       // the project we're scrolling to will always start with the cover,
@@ -34,44 +35,78 @@ export default class App extends React.Component {
       const projectElement = this.projectsContainer.children[this.projectIndex]
       projectElement.scrollLeft = 0
       this.pieceIndex = 0
-      scroll.top(this.projectsContainer, window.innerHeight * this.projectIndex, { duration: 500 })
+      this.isScrolling = true
+      scroll.top(this.projectsContainer, window.innerHeight * this.projectIndex, { duration: 500 }, finishedScrolling)
     }
 
     const horizontalScroll = () => {
       const projectElement = this.projectsContainer.children[this.projectIndex]
       const offset = projectElement.children[this.pieceIndex].offsetLeft
-      scroll.left(projectElement, offset, { duration: 500 })
+      this.isScrolling = true
+      scroll.left(projectElement, offset, { duration: 500 }, finishedScrolling)
+    }
+
+    const up = () => {
+      if (this.projectIndex > 0) {
+        this.projectIndex -= 1
+        verticalScroll()
+      }
+    }
+
+    const down = () => {
+      if (this.projectIndex < projects.length - 1) {
+        this.projectIndex += 1
+        verticalScroll()
+      }
+    }
+
+    const left = () => {
+      if (this.pieceIndex > 0) {
+        this.pieceIndex -= 1
+        horizontalScroll()
+      }
+    }
+
+    const right = () => {
+      const pieces = projects[this.projectIndex].frontmatter.pieces
+        if (this.pieceIndex < pieces.length) {
+          this.pieceIndex += 1
+          horizontalScroll()
+        }
     }
 
     window.addEventListener('keydown', (e) => {
       switch(e.key) {
         case 'ArrowUp':
-          if (this.projectIndex > 0) {
-            this.projectIndex -= 1
-            verticalScroll()
-          }
+          up()
         break
         case 'ArrowDown':
-          if (this.projectIndex < projects.length - 1) {
-            this.projectIndex += 1
-            verticalScroll()
-          }
+          down()
         break
         case 'ArrowLeft':
-          if (this.pieceIndex > 0) {
-            this.pieceIndex -= 1
-            horizontalScroll()
-          }
+          left()
         break
         case 'ArrowRight':
-          const pieces = projects[this.projectIndex].frontmatter.pieces
-          if (this.pieceIndex < pieces.length) {
-            this.pieceIndex += 1
-            horizontalScroll()
-          }
+          right()
         break
       }
     })
+
+    window.addEventListener('wheel', (e) => {
+      if (this.isScrolling) {
+        return
+      }
+
+      if (Math.abs(e.deltaY) > scrollTriggerThreshold) {
+        e.deltaY > 0 ? down() : up()
+      }
+
+      if (Math.abs(e.deltaX) > scrollTriggerThreshold) {
+        e.deltaX > 0 ? right() : left()
+      }
+      
+    }, { passive: true })
+
   }
 
   handleRef (el) {
