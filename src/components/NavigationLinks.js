a98a7a54ce28ec111b7652377e8bccf188edb4c2
Luke Barlow <luke.barlow@gmail.com>
2019-02-12 18:02:30 +0000

    navigation and info page working

---
+++ src/components/NavigationLinks.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+const Bar = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 0px;
+  white-space: nowrap;
+  color: ${({isWhite}) => isWhite ? 'white': 'black'}
+  transition: color 0.5s;
+
+  span {
+    display: inline-block;
+    margin-left: 30px;
+    cursor: pointer;
+  }
+`
+
+export default ({projects, onLink, isWhite}) => {
+  return <Bar isWhite={isWhite}>
+    { projects.map((p, i) => <span onClick={() => onLink(i)} key={i} project={p}>{p.frontmatter.title}</span>)}
+    <span onClick={() => onLink('info')}>Info</span>
+  </Bar>
+}
